ee92fc264ea0f356a78912abdaa300642c02611f
olexp9 <methasit.ka.65@ubu.ac.th>
2025-03-01 14:47:51 +0700

    1/3/2025

--- django/mysite/myapp/apps.py
+++ django/mysite/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StreamlitappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'streamlitapp'
+    name = 'myapp'
--- jango-streamlit/jango/streamlitapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UploadedFile, UserData
-
-# Register your models here.
-admin.site.register(UploadedFile)
-admin.site.register(UserData)
